a8bfa66723c506e90e2e96ced0608b309764c57a
Abel Papazian <apelpapa@gmail.com>
2026-09-18 18:54:10 -0400

    Add: unique-email-addresses - submission-1

---
+++ Data Structures & Algorithms/unique-email-addresses/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        uniqueEmail = {}
+        for email in emails:
+            tempEmail = email.split("@")
+            tempEmail[0] = tempEmail[0].replace(".","")
+            tempEmail2 = tempEmail[0].split("+")
+            newEmail = tempEmail2[0] + "@" + tempEmail[1]
+            uniqueEmail[newEmail] = 1
+        return len(uniqueEmail)
+        
