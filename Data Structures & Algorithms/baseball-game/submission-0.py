bce45d295aa16fd795ec64bd734bde4336a92403
Abel Papazian <apelpapa@gmail.com>
2026-09-21 17:43:19 -0400

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        tracker = []
+        for op in operations:
+            try: 
+                tracker.append(int(op))
+            except:
+                if op == "+":
+                    tracker.append(tracker[-1] + tracker[-2])
+                elif op == "D":
+                    tracker.append(tracker[-1]*2)
+                elif op == "C":
+                    tracker.pop()
+        sum_tracker = 0
+        for num in tracker:
+            sum_tracker += num
+        return sum_tracker
+        
