371694e24d3079fc0348796c610d91311bfeb61f
Abel Papazian <apelpapa@gmail.com>
2026-09-23 15:37:30 -0400

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.py
@@ -0,0 +1,37 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        time, fresh = 0, 0
+        rotten_queue = deque()
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2:
+                    rotten_queue.append([r, c])
+                elif grid[r][c] == 1:
+                    fresh += 1
+        
+        rot_directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+        while len(rotten_queue) > 0 and fresh > 0:
+            print("in while")
+            for rotten in range(len(rotten_queue)):
+                print("in for 1")
+                crr, crc = rotten_queue.popleft()
+                for tbr, tbc in rot_directions:
+                    print("in for 2")
+                    temp_r = crr + tbr
+                    temp_c = crc + tbc
+                    print(temp_r, temp_c)
+                    if temp_r < 0 or temp_c < 0 or temp_r >= len(grid) or temp_c >= len(grid[0]) or grid[temp_r][temp_c] != 1:
+                        print("in if 1")
+                        continue
+                    else:
+                        rotten_queue.append([crr + tbr, crc + tbc])
+                        grid[temp_r][temp_c] = 2
+                        print("in if 2")
+                        fresh -= 1
+                        print(fresh)
+            time += 1
+
+        if fresh > 0:
+            return -1
+        return time
